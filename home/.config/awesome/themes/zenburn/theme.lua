095f328304d1e572cc090c4ac39a36c63995588f
zackp30 <zack.piper123@gmail.com>
2014-04-19 00:17:05 +0100

    ..

--- home/.config/awesome/themes/zenburn/theme.lua
+++ home/.config/awesome/themes/zenburn/theme.lua
@@ -6,11 +6,60 @@
 -- Alternative icon sets and widget icons:
 --  * http://awesome.naquadah.org/wiki/Nice_Icons
 
+-- Libs {{{
+local blingbling = require("blingbling")
+-- }}}
 -- {{{ Main
 theme = {}
 theme.wallpaper = "/usr/share/awesome/themes/zenburn/zenburn-background.png"
 -- }}}
+-- Colours {{{
+local function rgb(red, green, blue)
+  if type(red) == "number" or type(green) == "number" or type(blue) == "number" then
+    return "#"..string.format("%02x",red)..string.format("%02x",green)..string.format("%02x",blue)
+  else    
+    return nil
+  end
+end
 
+local function rgba(red, green, blue, alpha) 
+  if type(red) == "number" or type(green) == "number" or type(blue) == "number" or type(alpha) == "number" then
+    return "#"..string.format("%02x",red)..string.format("%02x",green)..string.format("%02x",blue)..string.format("%02x",alpha * 255)
+  else
+    return nil
+  end
+end
+
+--colors
+local bordeaux= rgb(47,28,28)
+local light_bordeaux = rgba(191,64,64,0.6)
+local dark_grey = "#121212"
+local grey = "#444444ff"
+local light_grey = "#555555"
+local white = "#ffffff"
+local light_white = "#999999"
+local light_black = "#232323"
+local red = "#b9214f"
+local bright_red = "#ff5c8d"
+local yellow = "#ff9800"
+local bright_yellow = "#ffff00"
+local black = "#000000"
+local bright_black = "#5D5D5D"
+local green = "#A6E22E"
+local bright_green = "#CDEE69"
+local blue = "#3399ff"
+local bright_blue = "#9CD9F0"
+local magenta = "#8e33ff"
+local bright_magenta = "#FBB1F9"
+local cyan = "#06a2dc"
+local bright_cyan = "#77DFD8"
+local widget_background = "#303030"
+--local white = "#B0B0B0"
+local bright_white = "#F7F7F7"
+local transparent = "#00000000"
+--background=#121212
+--foreground=#aeafad
+-- }}}
 -- {{{ Styles
 theme.font      = "sans 8"
 
@@ -26,7 +75,7 @@ theme.bg_systray = theme.bg_normal
 
 -- {{{ Borders
 theme.border_width  = 2
-theme.border_normal = "#3F3F3F"
+theme.border_normal = "#F00000"
 theme.border_focus  = "#6F6F6F"
 theme.border_marked = "#CC9393"
 -- }}}
@@ -45,7 +94,6 @@ theme.titlebar_bg_normal = "#3F3F3F"
 -- Example:
 --theme.taglist_bg_focus = "#CC9393"
 -- }}}
-
 -- {{{ Widgets
 -- You can add as many variables as
 -- you wish and access them by using
@@ -56,12 +104,10 @@ theme.titlebar_bg_normal = "#3F3F3F"
 --theme.bg_widget        = "#494B4F"
 --theme.border_widget    = "#3F3F3F"
 -- }}}
-
 -- {{{ Mouse finder
 theme.mouse_finder_color = "#CC9393"
 -- mouse_finder_[timeout|animate_timeout|radius|factor]
 -- }}}
-
 -- {{{ Menu
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
@@ -69,7 +115,6 @@ theme.mouse_finder_color = "#CC9393"
 theme.menu_height = 15
 theme.menu_width  = 100
 -- }}}
-
 -- {{{ Icons
 -- {{{ Taglist
 theme.taglist_squares_sel   = "/usr/share/awesome/themes/zenburn/taglist/squarefz.png"
@@ -122,5 +167,45 @@ theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/zen
 theme.titlebar_maximized_button_normal_inactive = "/usr/share/awesome/themes/zenburn/titlebar/maximized_normal_inactive.png"
 -- }}}
 -- }}}
+-- BlingBling {{{
+theme.blingbling = {
+    background_color = "#00000000",
+    graph_background_color = widget_background,
+    graph_color = bright_magenta,
+    graph_line_color = magenta,
+    rounded_size = 0.3,        
+    text_color = light_white,
+    font = "Droid Sans Mono",
+    font_size = 9 
+}
+theme.blingbling.tagslist = {}
+theme.blingbling.tagslist.normal ={ background_color = widget_background,--rgb(26,26,26),
+                                    text_background_color = "#00000000", --no color
+                                    rounded_size = { 0, 0.4,0,0.4 },
+                                    text_color = theme.fg_normal,
+                                    font = "Droid Sans",
+                                    font_size = 7 
+                                  }
+theme.blingbling.tagslist.focus = { h_margin = 1,
+                                    v_margin = 1,
+                                    background_color = red, 
+                                    text_background_color = widget_background,
+                                    text_color = theme.fg_normal,
+                                    rounded_size = { 0, 0.4,0,0.4 },
+                                    font = "Droid Sans italic",
+                                    font_size = 8
+                                  }
 
+theme.blingbling.tagslist.urgent = theme.blingbling.tagslist.focus
+theme.blingbling.tagslist.occupied = theme.blingbling.tagslist.normal
+
+local cal_common_style = {  h_margin = 0, 
+                            v_margin = 0, 
+                            rounded_size = 0.3, 
+                            background_color = widget_background, 
+                            text_background_color = "#00000000",
+                            text_color = white, 
+                            font ="Droid Sans"
+                          }
+                          -- }}}
 return theme
