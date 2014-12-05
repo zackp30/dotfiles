e4542e6fb83eada417341d7ffd3475795fa30961
Zack Piper <zack.piper123@gmail.com>
2014-12-05 16:10:50 +0000

    Minor adjustments

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -2,6 +2,7 @@
 
 ;;; Code:
 
+
 (when (not (string= system-name "xieshaij"))
     (setq url-proxy-services '(("http" . "localhost:8123"))))
 
@@ -35,6 +36,7 @@
 (require-package 'color-theme-solarized)
 (require-package 'surround)
 (require-package 'auto-complete)
+(require-package 'highlight-numbers)
 (require-package 'magit)
 (require-package 'table)
 (require-package 'ac-dcd)
@@ -61,6 +63,7 @@
 (require-package 'git-messenger)
 (require-package 'perspective)
 (require-package 'smart-mode-line)
+(require-package 'langtool)
 (require-package 'yasnippet)
 (require-package 'helm)
 (require-package 'flycheck)
@@ -91,7 +94,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ac-ispell-fuzzy-limit 1)
- '(ac-ispell-requires 1)
  '(custom-safe-themes
    (quote
     ("756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
@@ -165,9 +167,9 @@
 (setq vc-follow-symlinks t)
 (global-set-key (kbd "C-x v p") 'git-messenger:popup-message)
 ;; Haskell!
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (autoload 'ghc-init "ghc" nil t)
 (add-hook 'haskell-mode-hook 'ghc-init)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 
 ;; Enable markdown-mode for .txt, .markdown, and .md
 (add-to-list 'auto-mode-alist 
@@ -251,6 +253,7 @@
 (load-theme 'solarized-dark)
 
 ;; Key bindings
+;;(global-set-key (kbd "C-TAB") )
 (global-set-key (kbd "C-c h") 'helm-projectile)
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
@@ -285,12 +288,12 @@
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
 (setq scss-compile-at-save nil)
 
+(require 'langtool)
+(setq langtool-language-tool-jar "~/LanguageTool-2.6/languagetool-commandline.jar")
+
+(add-hook 'text-mode-hook (lambda () (add-hook 'after-save-hook 'langtool-check)))
+
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
