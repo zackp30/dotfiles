23d843b74ff7efaa732312b277218a5405a57fd7
Zack Piper <zack.piper123@gmail.com>
2015-07-14 13:05:03 +0000

    Remove unneeded Vim folding things

--- home/.config/awesome/themes/zenburn/theme.lua
+++ home/.config/awesome/themes/zenburn/theme.lua
@@ -6,7 +6,6 @@
 -- Alternative icon sets and widget icons:
 --  * http://awesome.naquadah.org/wiki/Nice_Icons
 
--- {{{ Main
 theme = {}
 if os.getenv("HOSTNAME") == "raspberrypi" then
   theme.wallpaper = os.getenv("HOME").."/bg/pi.jpg"
@@ -15,11 +14,8 @@ else
   theme.wallpaper = os.getenv("HOME").."/.config/awesome/themes/zenburn/gnu.png"
 
 end
--- }}}
--- {{{ Styles
 theme.font      = "sans 8"
 
--- {{{ Colors
 theme.fg_normal  = "#DCDCCC"
 theme.fg_focus   = "#F0DFAF"
 theme.fg_urgent  = "#CC9393"
@@ -27,19 +23,14 @@ theme.bg_normal  = "#3F3F3F"
 theme.bg_focus   = "#1E2320"
 theme.bg_urgent  = "#3F3F3F"
 theme.bg_systray = theme.bg_normal
--- }}}
 
--- {{{ Borders
 theme.border_width  = 2
 theme.border_focus = "#03C5E7"
 theme.border_normal  = "#202020"
 theme.border_marked = "#CC9393"
--- }}}
 
--- {{{ Titlebars
 theme.titlebar_bg_focus  = "#3F3F3F"
 theme.titlebar_bg_normal = "#3F3F3F"
--- }}}
 
 -- There are other variable sets
 -- overriding the default one when
@@ -49,8 +40,6 @@ theme.titlebar_bg_normal = "#3F3F3F"
 -- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
 -- Example:
 --theme.taglist_bg_focus = "#CC9393"
--- }}}
--- {{{ Widgets
 -- You can add as many variables as
 -- you wish and access them by using
 -- beautiful.variable in your rc.lua
@@ -59,31 +48,20 @@ theme.titlebar_bg_normal = "#3F3F3F"
 --theme.fg_end_widget    = "#FF5656"
 --theme.bg_widget        = "#494B4F"
 --theme.border_widget    = "#3F3F3F"
--- }}}
--- {{{ Mouse finder
 theme.mouse_finder_color = "#CC9393"
 -- mouse_finder_[timeout|animate_timeout|radius|factor]
--- }}}
--- {{{ Menu
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
 theme.menu_height = 15
 theme.menu_width  = 100
--- }}}
--- {{{ Icons
--- {{{ Taglist
 theme.taglist_squares_sel   = "/usr/share/awesome/themes/zenburn/taglist/squarefz.png"
 theme.taglist_squares_unsel = "/usr/share/awesome/themes/zenburn/taglist/squarez.png"
 --theme.taglist_squares_resize = "false"
--- }}}
 
--- {{{ Misc
 theme.awesome_icon           = "/usr/share/awesome/themes/zenburn/awesome-icon.png"
 theme.menu_submenu_icon      = "/usr/share/awesome/themes/default/submenu.png"
--- }}}
 
--- {{{ Layout
 theme.layout_tile       = "/usr/share/awesome/themes/zenburn/layouts/tile.png"
 theme.layout_tileleft   = "/usr/share/awesome/themes/zenburn/layouts/tileleft.png"
 theme.layout_tilebottom = "/usr/share/awesome/themes/zenburn/layouts/tilebottom.png"
@@ -96,9 +74,7 @@ theme.layout_max        = "/usr/share/awesome/themes/zenburn/layouts/max.png"
 theme.layout_fullscreen = "/usr/share/awesome/themes/zenburn/layouts/fullscreen.png"
 theme.layout_magnifier  = "/usr/share/awesome/themes/zenburn/layouts/magnifier.png"
 theme.layout_floating   = "/usr/share/awesome/themes/zenburn/layouts/floating.png"
--- }}}
 
--- {{{ Titlebar
 theme.titlebar_close_button_focus  = "/usr/share/awesome/themes/zenburn/titlebar/close_focus.png"
 theme.titlebar_close_button_normal = "/usr/share/awesome/themes/zenburn/titlebar/close_normal.png"
 
@@ -121,9 +97,6 @@ theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/zenbu
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/zenburn/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/zenburn/titlebar/maximized_focus_inactive.png"
 theme.titlebar_maximized_button_normal_inactive = "/usr/share/awesome/themes/zenburn/titlebar/maximized_normal_inactive.png"
--- }}}
--- }}}
--- BlingBling {{{
 theme.blingbling = {}
 theme.blingbling.tagslist = {}
 theme.blingbling.tagslist.normal = { background_color = "#1AB7FF",
@@ -145,5 +118,4 @@ theme.blingbling.tagslist.urgent = { background_color = "#1AB7FF",
                                     text_background_color = "#00000000",
                                     text_color = "#F0F0F0",
                                   }
--- }}}
 return theme
