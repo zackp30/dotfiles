ab4a406615e2509f433771e7aad17f05dedfbf57
Zack Piper <zack.piper123@gmail.com>
2015-07-18 11:23:43 +0000

    Fix save-theme saving

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -41,14 +41,7 @@
                 (delete-file (concat "/tmp/emacs" (number-to-string (user-uid)) "/ready/" server-name)))))
   (append-to-file (number-to-string (emacs-pid)) nil pid-file))
 (add-hook 'after-init-hook 'init-pid)
-(require 'ob-tangle)
-(org-babel-load-file "~/.emacs.d/config.org")
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -61,6 +54,15 @@
  '(package-selected-packages
    (quote
     (material-theme yasnippet yaml-mode ws-butler workgroups2 wgrep-ag wgrep web-mode wanderlust todotxt sx surround spinner smart-mode-line slime-company slime slim-mode scss-mode rust-mode rainbow-identifiers rainbow-delimiters rainbow-blocks racket-mode perspective ocodo-svg-modelines nim-mode mmm-mode mediawiki zenburn-theme markdown-mode magit-tramp magit lua-mode lentic ledger-mode julia-mode js2-mode io-mode indent-guide ibuffer-vc hydra hy-mode highlight-numbers helm-swoop helm-projectile helm-ag go-mode gnuplot-mode gitignore-mode gitconfig-mode git-timemachine git-gutter git-commit-mode gist ghci-completion ggtags flycheck-rust flycheck-ledger flycheck evil-visualstar evil-numbers evil-nerd-commenter evil-matchit evil-leader evil-indent-textobject evil etags-select emmet-mode emacs-eclim elixir-mode ein editorconfig edit-server dired-toggle-sudo dired-rainbow dired+ d-mode ctags company-tern company-ghc company-anaconda company coffee-mode cmake-mode cider browse-kill-ring bookmark+ ag ace-window ace-jump-helm-line ace-flyspell use-package))))
+
+(require 'ob-tangle)
+(org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 (load-theme 'zenburn t)
 
