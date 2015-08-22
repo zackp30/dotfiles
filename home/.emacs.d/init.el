0b83a699889a018a6e0451e1b076c26b3bc1d060
Zack Piper <zack.piper123@gmail.com>
2015-08-22 00:13:34 +0000

    Nuke emacs-servers

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -6,57 +6,11 @@
 (setq vc-follow-symlinks t)
 
 ;; Server handling
-;; I use multiple Emacs servers, and had a problem where loading `config.org'
-;; and tangling it into `config.el' ended up causing a lock.
-;; So I went on the quest to make my own system to run them one at a time.
-;; This involved:
-;; 1. Running the servers in a contained manner (tmux!).
-;; 2. Wait for a PID file to appear, which will happen a few seconds after `after-init-hook'
-;; 3. Make Emacs servers depend on each other, for example:
-;;   - fb-mapper depends on documents which depends on dotfiles which depends on server
-;;
-;; This system works really well.
-
 (defun shell-without-newline (s)
   "Remove the newline from the output of a shell command.
 S: shell command to run"
   (replace-regexp-in-string "\n$" ""
                             (shell-command-to-string s)))
-(when (getenv "EMACS_USE_CASCADE")
-  (defvar project-name (getenv "PROJECT_NAME")
-    "Get the project that Emacs is running within.")
-
-  (defvar pid-dir (concat "/tmp/emacs" (number-to-string (user-uid)) "/ready/")
-    "Where the PIDs are stored.")
-  (setq server-name project-name)
-  (defvar pid-file (concat pid-dir server-name)
-    "Where the PID file is.")
-
-  ;; Here we determine whether we're about to conflict with another Emacs server.
-  (when (string= "yes" (shell-without-newline (concat
-                                               "isemacsrunning "
-                                               server-name
-                                               " "
-                                               (number-to-string 1))))
-    (progn (error (concat "Emacs server (" server-name ") is already running, exiting"))
-           (kill-emacs)))
-
-  (when (not server-mode)
-    (server-start))
-
-
-  (defun init-pid ()
-    "saves a PID file"
-    (when (file-exists-p pid-file)
-      (delete-file pid-file))
-    (when (not (file-exists-p pid-dir))
-      (make-directory pid-dir t))
-    (when (file-exists-p pid-file)
-      (add-hook 'kill-emacs-hook
-                (lambda ()
-                  (delete-file (concat "/tmp/emacs" (number-to-string (user-uid)) "/ready/" server-name)))))
-    (append-to-file (number-to-string (emacs-pid)) nil pid-file))
-  (add-hook 'after-init-hook 'init-pid))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -84,4 +38,8 @@ S: shell command to run"
  ;; If there is more than one, they won't work right.
  )
 
+(server-start)
+
 (load-theme 'material t)
+
+
