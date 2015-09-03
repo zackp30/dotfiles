915b7a8c42c211f4b156cbabde1888564b1ee168
Zack Piper <zack.piper123@gmail.com>
2015-09-03 17:34:14 +0200

    Italicize comment face, add prettyify symbols

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -19,7 +19,7 @@ S: shell command to run"
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("26614652a4b3515b4bbbb9828d71e206cc249b67c9142c06239ed3418eff95e2" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "26614652a4b3515b4bbbb9828d71e206cc249b67c9142c06239ed3418eff95e2" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
  '(indent-guide-char "│")
  '(org-agenda-files
    (quote
@@ -36,7 +36,7 @@ S: shell command to run"
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(font-lock-comment-face ((t (:foreground "#7F9F7F" :slant italic)))))
 
 (server-start)
 
