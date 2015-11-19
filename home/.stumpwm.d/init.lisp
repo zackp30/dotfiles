4887e6475a038c2e5755350e61c36c533250fb56
Zack Piper <zack.piper123@gmail.com>
2015-11-19 18:41:32 +0100

    Add fprev, like fnext but goes backwards to StumpWM
    Extracted from a patch on the ML:
    
    https://lists.gnu.org/archive/html/stumpwm-devel/2008-11/msg00009.html

--- home/.stumpwm.d/init.lisp
+++ home/.stumpwm.d/init.lisp
@@ -18,9 +18,17 @@
 
 (define-key *root-map* (kbd "c") "exec urxvt-256color") ;; terminal
 (define-key *root-map* (kbd "C-c") "chromium") ;; browser
+
+(define-key *root-map* (kbd "C-z") "fnext")
+(define-key *root-map* (kbd "C-x") "fprev")
+
 (define-key *root-map* (kbd "C-q") "reinit") ;; reload config
 (define-key *root-map* (kbd "p") "rofi") ;; dmenu like utility; TODO: Doesn't work on X2Go, File a bug report?
 
 (setq *mouse-focus-policy* :sloppy)
 
 (loop for i from 0 to 9 do (run-commands (format nil "gnewbg ~a" i))) ;; 10 workspaces
+
+(defcommand (fprev tile-group) () ()
+            "Cycle through the frame tree to the prev frame."
+            (focus-prev-frame (current-group)))
