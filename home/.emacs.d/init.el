b99f260ed2eeea595ae0f26dde480581b9c70ad6
Zack Piper <zack.piper123@gmail.com>
2015-11-22 09:45:58 +0100

    Try a different keybinding for `evil-toggle-key`

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -26,7 +26,7 @@ S: shell command to run"
  '(custom-safe-themes
    (quote
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "26614652a4b3515b4bbbb9828d71e206cc249b67c9142c06239ed3418eff95e2" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
- '(evil-toggle-key "C-x C-x")
+ '(evil-toggle-key "M-z")
  '(indent-guide-char "│")
  '(org-agenda-files
    (quote
