57f301ec0010e912fe463738ae20f8fbf111ad3b
Zack Piper <zack.piper123@gmail.com>
2015-12-26 16:44:04 +0000

    The commit message was empty

--- home/.stumpwm.d/rules/gimp-rules.lisp
+++ home/.stumpwm.d/rules/gimp-rules.lisp
@@ -1,3 +1,3 @@
 ((#1="gimp" 0. t t :CLASS "Gimp" :INSTANCE "gimp" :ROLE "gimp-toolbox")
- (#1# 1. t t :CLASS "Gimp" :INSTANCE "gimp" :ROLE "gimp-dock")
- (#1# 2. t t :CLASS "Gimp" :INSTANCE "gimp"))
+ (#1# 2. t t :CLASS "Gimp" :INSTANCE "gimp" :ROLE "gimp-dock")
+ (#1# 1. t t :CLASS "Gimp" :INSTANCE "gimp"))
