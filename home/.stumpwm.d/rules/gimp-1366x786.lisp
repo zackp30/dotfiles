c1dff0bb215750b79d16ee16bb95b2c9436061d0
Zack Piper <zack.piper123@gmail.com>
2015-12-26 16:35:41 +0000

    The commit message was empty

---
+++ home/.stumpwm.d/rules/gimp-1366x786.lisp
@@ -0,0 +1,28 @@
+#S(GDUMP
+   :NUMBER 8
+   :NAME "6"
+   :TREE (((#S(FDUMP
+               :NUMBER 0
+               :X 0
+               :Y 0
+               :WIDTH 413
+               :HEIGHT 384
+               :WINDOWS (44040614)
+               :CURRENT 44040614)
+            #S(FDUMP
+               :NUMBER 2
+               :X 0
+               :Y 384
+               :WIDTH 413
+               :HEIGHT 384
+               :WINDOWS (44040594)
+               :CURRENT 44040594))
+           #S(FDUMP
+              :NUMBER 1
+              :X 413
+              :Y 0
+              :WIDTH 953
+              :HEIGHT 768
+              :WINDOWS (44040552)
+              :CURRENT 44040552)))
+   :CURRENT 1)
