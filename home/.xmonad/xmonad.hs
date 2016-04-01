445234736b39e3f9a4abeef40eaa20e54bf6fc59
Zack Piper <zack.piper123@gmail.com>
2016-04-01 14:51:39 +0100

    The commit message was empty

--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -1,61 +1,59 @@
-import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run (spawnPipe)
-import XMonad.Util.EZConfig (additionalKeys)
-import XMonad.Util.SpawnOnce
-import XMonad.Actions.GridSelect
-import XMonad.Layout.Accordion
-import XMonad.Layout.Tabbed
-import XMonad.Layout.PerWorkspace
+import           System.Exit
+import           XMonad
+import           XMonad.Hooks.DynamicLog
+import           XMonad.Hooks.ManageDocks
+import           XMonad.Hooks.ManageHelpers
+import           XMonad.Layout.Fullscreen
+import           XMonad.Layout.LayoutHints
+import           XMonad.Layout.NoBorders
+import           XMonad.Prompt
+import           XMonad.Prompt.ConfirmPrompt
+import           XMonad.Prompt.Shell
+import           XMonad.Util.EZConfig
+import           XMonad.Util.Run(hPutStrLn, spawnPipe)
 
-import System.IO
+modKey = mod4Mask
 
-tabConfig = defaultTheme {
-    -- from https://github.com/vicfryzel/xmonad-config/blob/master/xmonad.hs
-    activeBorderColor = "#7C7C7C",
-    activeTextColor = "#CEFFAC",
-    activeColor = "#000000",
-    inactiveBorderColor = "#7C7C7C",
-    inactiveTextColor = "#EEEEEE",
-    inactiveColor = "#000000"
-}
-
-myManageHook = composeAll
-    [ className =? "stalonetray" --> doIgnore
-    , className =? "switcher" --> doIgnore
-    , manageDocks ]
-myMod = mod1Mask
-myLayout = tiled ||| Mirror tiled ||| Full ||| tabbed shrinkText tabConfig
-  where
-    -- default tiling algorithm partitions the screen into two panes
-    tiled   = Tall nmaster delta ratio
-
-    -- The default number of windows in the master pane
-    nmaster = 1
-
-    -- Default proportion of screen occupied by master pane
-    ratio   = 1/2
-
-    -- Percent of screen to increment by when resizing panes
-    delta   = 3/100
-myTerminal = "urxvt-256color"
 
 main = do
-  xmproc <- spawnPipe "xmobar ~/.xmobarrc"
-  xmonad $ defaultConfig {
-         logHook = dynamicLogWithPP xmobarPP
-                         { ppOutput = hPutStrLn xmproc,
-                           ppTitle = xmobarColor "green" "" . shorten 50
-                         },
-         manageHook = manageDocks <+> manageHook defaultConfig,
-         layoutHook = avoidStruts myLayout,
-         terminal = myTerminal,
-         borderWidth = 3,
-         focusedBorderColor = "#03C5E7"
-    } `additionalKeys`
-    [ ((myMod, xK_BackSpace), goToSelected defaultGSConfig)
-    , ((myMod .|. shiftMask, xK_n), sendMessage NextLayout)
-    , ((myMod .|. shiftMask, xK_p), sendMessage FirstLayout)
-    , ((myMod, xK_Return), spawn myTerminal)
-    , ((myMod, xK_p), spawn "rofi -show run")]
+    xmproc <- spawnPipe "~/bin/xmobar ~/.xmobarrc"
+    xmonad $ defaultConfig
+       {
+          terminal = "urxvt-256color"
+        , manageHook = myManageHook <+> fullscreenManageHook <+> manageDocks
+                       <+> manageHook defaultConfig
+        , modMask = modKey
+        , layoutHook = fullscreenFloat $ fullscreenFocus $ layoutHints
+                       $ smartBorders $ avoidStruts $ layoutHook defaultConfig
+        , handleEventHook = fullscreenEventHook <+> docksEventHook
+        , logHook = dynamicLogWithPP $ xmobarPP { ppOutput = hPutStrLn xmproc }
+       }
+       `additionalKeysP`
+         [
+            -- Ask for confirmation before exiting by replacing the default
+            -- binding with a custom one.
+            ("M-S-q", confirmPrompt myXPConfig "exit" $ io exitSuccess)
+            -- Replace M-p launcher binding with built-in shellPrompt, which
+            -- is friendlier and does not require dmenu.
+          , ("M-p", shellPrompt myXPConfig)
+         ]
+       `additionalKeys`
+        [ ((modKey, xK_q), spawn "xmonad --recompile && xmonad --restart") ]
+
+-- see http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Prompt.html#t:XPConfig
+myXPConfig = defaultXPConfig
+    {
+      position = Top
+    , promptBorderWidth = 0
+    , defaultText = ""
+    , alwaysHighlight = True
+    , font = "9x15"
+    }
+
+-- see http://xmonad.org/xmonad-docs/xmonad/XMonad-ManageHook.html
+myManageHook = composeAll
+    [
+      className =? "Pidgin"             --> doFloat
+    , className =? "XCalc"              --> doFloat
+    , className =? "mpv"                --> doFloat
+    ]
