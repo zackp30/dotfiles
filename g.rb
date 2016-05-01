6c425167d6fc4fb91aa9dd5c85bacf615c387cb6
Zack Piper <zack.piper123@gmail.com>
2016-05-01 17:40:39 +0000

    The commit message was empty

---
+++ g.rb
@@ -0,0 +1,6 @@
+$settings = {
+  whitespace: {
+    exts: %w(md org),
+    exceptions: %w(home/.zsh/plugins/)
+  }
+}
---
+++ spec/whitespace_spec.rb
@@ -0,0 +1,18 @@
+require './g.rb'
+
+describe 'File' do
+  Dir.glob("**/*.{#{$settings[:whitespace][:exts].join(',')}}", File::FNM_DOTMATCH).each do |g|
+next if
+    describe g do
+      line = 0
+      IO.foreach g do |l|
+        line += 1
+        describe "Line #{line}" do
+          it "should have no trailing whitespace" do
+            expect(l.sub(/[ \t]+$/, '')).to eql(l)
+          end
+        end
+      end
+    end
+  end
+end
