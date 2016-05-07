d6ba84fb09bf603aaca6501d1b9ae92aa6075fd3
Zack Piper <zack.piper123@gmail.com>
2016-05-07 13:08:09 +0000

    The commit message was empty

--- spec/whitespace_spec.rb
+++ spec/whitespace_spec.rb
@@ -2,13 +2,12 @@ require './g.rb'
 
 describe 'File' do
   $settings[:whitespace][:files].each do |g|
-    next if
     describe g do
       line = 0
       IO.foreach g do |l|
         line += 1
         describe "Line #{line}" do
-          it "should have no trailing whitespace" do
+          it 'should have no trailing whitespace' do
             expect(l.sub(/[ \t]+$/, '')).to eql(l)
           end
         end
