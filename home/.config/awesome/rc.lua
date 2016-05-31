8c1048503691399791fe9353237dc0ec89ac5019
Zack Piper <zack.piper123@gmail.com>
2016-05-31 15:14:09 +0000

    The commit message was empty

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -91,6 +91,8 @@ end
 
 
 
+tags = {}
+
 awful.screen.connect_for_each_screen(function(s)
     -- Wallpaper
     if beautiful.wallpaper then
@@ -101,18 +103,9 @@ awful.screen.connect_for_each_screen(function(s)
         end
         gears.wallpaper.maximized(wallpaper, s, true)
     end
-
-    -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
-end)
-
-
--- Define a tag table which hold all screen tags.
-tags = {}
-awful.screen.connect_for_each_screen(function(s)
-   -- Each screen has its own tag table.
    tags[s] = awful.tag({ "♨", "⌨", "⚡", "✉", "☕", "❁", "☃", "☄", "⚢" }, s, awful.layout.layouts[1])
 end)
+
 
 
 
