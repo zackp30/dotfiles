ac8d4c7a01b8642eac49e9fc490867edf93df894
Zack Piper <zack.piper123@gmail.com>
2016-06-26 14:37:32 +0100

    The commit message was empty

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -305,6 +305,8 @@ globalkeys = awful.util.table.join(
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
               {description = "open a terminal", group = "launcher"}),
+    awful.key({ modkey,           }, ";", function () awful.spawn("rofi -show drun") end,
+              {description = "run desktop program", group = "launcher"}),
     awful.key({ modkey, "Control" }, "r", awesome.restart,
               {description = "reload awesome", group = "awesome"}),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
