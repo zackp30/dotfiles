aa0fa33f25a217bb05b5576a4a35a651ddf82c80
Zack Piper <zack.piper123@gmail.com>
2016-11-06 17:19:26 +0000

    The commit message was empty

--- spec/verlib.rb
+++ spec/verlib.rb
@@ -1,7 +1,15 @@
 class Ver
-  def initialize(version)
-    @version = version.split('.').map(&:to_i)
+  def t_semver(version)
+    @version = version
+  end
+  def initialize(type, version)
+    case type
+    when "semver"
+      t_semver version
+    else
+      throw "Invalid error type"
+    end
   end
 end
 
-puts Ver.new('1.2.3').inspect
+puts Ver.new('semver', '1.2.3').inspect
