b32422069032397304600737dfd0a786ec459fba
Zack Piper <zack.piper123@gmail.com>
2017-01-01 23:10:26 +0000

    The commit message was empty

--- home/.wallpaper/r.sh
+++ home/.wallpaper/r.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find $HOME/.wallpaper -name '*.jpg' -o -name '*.png' | shuf -n 1 | xargs feh --bg-scale
+find $HOME/.wallpaper -name '*.jpg' -o -name '*.png' | shuf -n 3 | xargs feh --bg-scale
