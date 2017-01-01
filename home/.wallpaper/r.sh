7d113f5ea558b050b33816fb1bd8e82babce9ba3
Zack Piper <zack.piper123@gmail.com>
2017-01-01 23:04:53 +0000

    -

--- home/.wallpaper/r.sh
+++ home/.wallpaper/r.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find $HOME/.wallpaper -type f -name '*.jpg' -o -name '*.png' | shuf -n 1 | xargs feh --bg-scale
+find $HOME/.wallpaper -name '*.jpg' -o -name '*.png' | shuf -n 1 | xargs feh --bg-scale
